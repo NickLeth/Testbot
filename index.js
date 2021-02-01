be5f113200282a07b0f7c647c957659707a8f830
NickLeth <69474498+NickLeth@users.noreply.github.com>
2021-02-01 17:19:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.on('guildMemberAdd', guildMember => {
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Ldier (lvl. 0)');
+
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('800607041801682954').send(`Velkommen til <@${guildMember.user.id}>`)
+});
+
+client.login('NTg2OTc1NzMyNzU3MTY4MTI4.XPv1wA.A27ShIhRPYXQU0TNZOt7wX0b3DY');
